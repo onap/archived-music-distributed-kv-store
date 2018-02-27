5031a4e20a281b7a94f8c3743b1572314c574ec4
Shashank Kumar Shankar <shashank.kumar.shankar@intel.com>
2018-02-27 15:41:46 -0800

    Add Docker building scripts and update swagger
    This patch adds scripts to build Docker container and
    updates swagger API doc.
    
    Change-Id: Ifb10d483946112e38d716c9ae07995ffeb14d2d3
    Issue-ID: MUSIC-41
    Signed-off-by: Shashank Kumar Shankar <shashank.kumar.shankar@intel.com>

---
+++ deployment/docker-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $HTTP_PROXY ]; then
+    BUILD_ARGS+=" --build-arg HTTP_PROXY=${HTTP_PROXY}"
+fi
+if [ $HTTPS_PROXY ]; then
+    BUILD_ARGS+=" --build-arg HTTPS_PROXY=${HTTPS_PROXY}"
+fi
+
+echo "Start build docker image"
+docker build ${BUILD_ARGS} -t dkv .
