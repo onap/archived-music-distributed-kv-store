245e5195b92f78d0202f900f351f17241b340c50
Shashank Kumar Shankar <shashank.kumar.shankar@intel.com>
2018-03-15 15:21:15 -0700

    Add Docker push to nexus support
    This patch adds support to push built docker container to
    Nexus.
    
    Change-Id: I9c9d3c01c548c0d3dcfec70f299765f0221379dc
    Issue-ID: MUSIC-59
    Signed-off-by: Shashank Kumar Shankar <shashank.kumar.shankar@intel.com>

--- deployment/docker-build.sh
+++ deployment/docker-build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+BUILD_ARGS="--no-cache"
+ORG="onap"
+VERSION="1.0.0"
+PROJECT="music"
+IMAGE="distributed-kv-store"
+DOCKER_REPOSITORY="nexus3.onap.org:10003"
+IMAGE_NAME="${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/${IMAGE}"
+TIMESTAMP=$(date +"%Y%m%dT%H%M%S")
+
 if [ $HTTP_PROXY ]; then
     BUILD_ARGS+=" --build-arg HTTP_PROXY=${HTTP_PROXY}"
 fi
@@ -7,5 +16,15 @@ if [ $HTTPS_PROXY ]; then
     BUILD_ARGS+=" --build-arg HTTPS_PROXY=${HTTPS_PROXY}"
 fi
 
-echo "Start build docker image"
-docker build ${BUILD_ARGS} -t dkv .
+function build_image {
+    echo "Start build docker image."
+    docker build ${BUILD_ARGS} -t ${IMAGE_NAME}:latest .
+}
+
+function push_image {
+    echo "Start push docker image."
+    docker push ${IMAGE_NAME}:latest
+}
+
+build_image
+push_image
