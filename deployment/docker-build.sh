2769c5195c1627849692a0542b9bdd9fef5fad08
Lasse Kaihlavirta <l.kaihlavirt@partner.samsung.com>
2020-11-05 18:01:29 +0200

    Initial test commit for project-specific CSIT
     - move music-distributed-kv-store CSIT to its own project repository
     - add execution script that copies required common run scripts from
       integration/csit repo to local workspace and runs the tests
     - add the possibility to provide additional docker tag as parameter
       to the build script
     - add script for tagging specified docker image with STAGING
    Issue-ID: INT-1769
    Signed-off-by: Lasse Kaihlavirta <l.kaihlavirt@partner.samsung.com>
    Change-Id: I19a5908f3c9c77529c43af0d89baf75a8f8557ee

--- deployment/docker-build.sh
+++ deployment/docker-build.sh
@@ -2,13 +2,14 @@
 
 BUILD_ARGS="--no-cache"
 ORG="onap"
-VERSION="1.0.0"
 PROJECT="music"
 IMAGE="distributed-kv-store"
 DOCKER_REPOSITORY="nexus3.onap.org:10003"
 IMAGE_NAME="${DOCKER_REPOSITORY}/${ORG}/${PROJECT}/${IMAGE}"
-TIMESTAMP=$(date +"%Y%m%dT%H%M%S")
+TAG_NAME=${UNIQUE_DOCKER_TAG}
 
+echo TAG_NAME: ${TAG_NAME}
+set -x
 if [ $HTTP_PROXY ]; then
     BUILD_ARGS+=" --build-arg HTTP_PROXY=${HTTP_PROXY}"
 fi
@@ -34,6 +35,10 @@ function build_image {
 function push_image {
     echo "Start push docker image."
     docker push ${IMAGE_NAME}:latest
+    if [ ! -z "${TAG_NAME}" ]; then
+      docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:1.0-SNAPSHOT-${TAG_NAME}
+      docker push ${IMAGE_NAME}:1.0-SNAPSHOT-${TAG_NAME}
+    fi
 }
 
 function remove_binary {
